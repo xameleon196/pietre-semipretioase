52399866be55cc6007703eea352f1b34e8a2657e
xameleon196 <64095973+xameleon196@users.noreply.github.com>
2024-11-13 08:23:47 +0200

    Delete conf.py

--- conf.py
+++
@@ -1,8 +0,0 @@
-# Minimal configuration for Read the Docs
-project = 'My Article Project'
-author = 'Your Name'
-release = '0.1'
-extensions = []
-templates_path = ['_templates']
-exclude_patterns = []
-html_theme = 'alabaster'
