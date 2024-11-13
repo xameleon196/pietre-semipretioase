b820e466ce6c63156455e760fdf5b0e7d1955a48
xameleon196 <64095973+xameleon196@users.noreply.github.com>
2024-11-13 08:21:19 +0200

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,8 @@
+# Minimal configuration for Read the Docs
+project = 'My Article Project'
+author = 'Your Name'
+release = '0.1'
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+html_theme = 'alabaster'
